3fe0f118ae21d495241fa6339015de1bd0cc59b1
ejasaityte <55398111+ejasaityte@users.noreply.github.com>
2020-02-20 18:27:24 +0000

    Update Library.java

--- src/dundee/ducklake/Library.java
+++ src/dundee/ducklake/Library.java
@@ -3,14 +3,37 @@ package dundee.ducklake;
 public class Library {
 
     public int[] books;
-
+    
+    public int numberOfBooks;
+    
     public int signupDays;
 
     public int booksPerDay;
 
-    public Library(int[] books, int signupDays, int booksPerDay) {
+    public Library(int[] books, int numberOfBooks, int signupDays, int booksPerDay) {
         this.books = books;
+        this.numberOfBooks=numberOfBooks;
         this.signupDays = signupDays;
         this.booksPerDay = booksPerDay;
+    }
+    
+    public void removeBookFromList(int bookID)
+    {
+    	for(int i=0; i<numberOfBooks; i++)
+    	{
+    		if(books[i]==bookID)
+    		{
+    			for(int j=i; j<numberOfBooks-1; j++)
+    			{
+    				books[j]=books[j+1];
+    			}
+    			numberOfBooks--;
+    		}
+    	}
+    }
+    
+    public void dayPassed()
+    {
+    	signupDays--;
     }
 }
